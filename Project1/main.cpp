0f49fa0c8cc3680f2ce1390f7fd38eb6af07692b
pederlh <36080334+pederlh@users.noreply.github.com>
2021-03-21 15:08:19 +0100

    Kommentarer og la til params fra fil i main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,21 +1,40 @@
 #include "Solver.hpp"
 
 int main(int argc, char const *argv[]) {
+    /*
+    int num_alphas, num_particles, dimensions, mc_cycles, mc_cycles_optimal_run, type_energy, type_sampling, num_treads;
+    if( argc != 9 ){
+        cout << "-------------------------------------------------------" << endl;
+        cout << "Bad Usage: " << argv[0] << " takes in eight integer arguments:" << endl;
+        cout << "num_alphas, num_particles, dimensions, mc_cycles, mc_cycles_optimal_run, type_energy, type_sampling, num_treads" << endl;            cout << "-------------------------------------------------------" << endl;
+        exit(1);
+    }
+    else{
+        num_alphas              = atoi(argv[1]);    // Number of alpha values to be looped over (ignored when gradient descent used)
+        num_particles           = atoi(argv[2]);    // Number of particles in system
+        dimensions              = atoi(argv[3]);    // Number of spacial dimensions in system
+        mc_cycles               = atoi(argv[4]);    // MC cycles used per run during gradient descent (GD) and in non-GD cases
+        mc_cycles_optimal_run   = atoi(argv[5]);    // MC cycles used in big run after gradient descent
+        type_energy             = atoi(argv[6]);    // type_energy = 0 for analytical, = 1 for brute force (Laplace operator in Hamiltonian).
+        type_sampling           = atoi(argv[7]);    // Chooses how MC sampling and search for optimal alpha is done. See instructions below
+        num_treads              = atoi(argv[8]);    // Number of threads (parallelization)
 
-    string outfilename, calc;
+        // Choices for type_sampling parameter:
+        // type_sampling = 0 for no importance sampling (loop through alpha-values)
+        // type_sampling = 1 for importance sampling (loop through alpha-values)
+        // type_sampling = 2 for importance sampling + gradient descent (non-interacting case)
+        // type_sampling = 3 for importance sampling + gradient descent (interacting case)        
+    }
+    */     
     int num_alphas = 15;
     int num_particles = 10;
     int mc_cycles = 1000;
     int mc_cycles_optimal_run = pow(2,17);
     int dimentions = 3;
-
-    // type_energy = 0 for analytical, type_energy = 1 for brute force.
     int type_energy = 0;
-
-    //type_sampling = 0 for no importance sampling, type_sampling = 1 for importance sampling, = 2 for Gradient descent + importance sampling, = 3 for interaction GD
     int type_sampling = 2;
 
-    if(type_energy ==1 && type_sampling==3){
+    if(type_energy == 1 && type_sampling == 3){
         cout << "Brute force energy calculation not implemented for interactive case"<<endl;
         return 1;
     }
@@ -36,22 +55,20 @@ int main(int argc, char const *argv[]) {
     {
         int ID = omp_get_thread_num();
 
-
-
-        // ------------------------------------------------
-        // OBS OBS Når skal vi starte og slutte å måle tid?
-        start_time = omp_get_wtime();                          // Start recording time
-        // ------------------------------------------------
-
+        // For the cases without gradient descent, we time the calculations from initializing objects to before writing results to file.
+        start_time = omp_get_wtime();                           // Start recording time
 
         // Initialize Solver object and perform calculations
         Solver mysolver(num_particles, num_alphas, mc_cycles, mc_cycles_optimal_run, dimentions, type_energy, type_sampling, ID);
 
-
-        end_time = omp_get_wtime();
+        end_time = omp_get_wtime();                             // Stop recording time
         double timeused = end_time - start_time;
 
-        if (type_energy==0) calc = "ana";
+
+        // When type_sampling is 0 or 1, data is written to file with the functions below
+
+        string calc, outfilename;
+        if (type_energy==0) calc = "ana";   // Different strings appended to file name
         if (type_energy==1) calc = "num";
 
         if (type_sampling == 0){
