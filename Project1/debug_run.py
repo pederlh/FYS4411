727cd6d6897618069cc8e8efdce4669339766f5f
Kaspara Skovli Gåsvær <kasparag@gmail.com>
2021-03-23 16:07:46 +0100

    changed some variables

--- Project1/debug_run.py
+++ Project1/debug_run.py
@@ -19,12 +19,12 @@ os.system("echo Compiling finished. Executing...")
 
 num_particles = 4
 dimentions = 3
-mc_cycles = 1000
+mc_cycles = 10000
 type_energy = 0
-type_sampling = 3
+type_sampling = 2
 num_threads = 1
 OBD_check = 0
-mc_cycles_optimal_run = 2**19 #/num_particles
+mc_cycles_optimal_run = 2**17 #/num_particles
 
 os.system("./main.out " + str(num_particles) + " " + str(dimentions) + " " + str(mc_cycles) + " " + str(type_energy) + " " \
                         + str(type_sampling) + " " + str(num_threads) + " " + str(OBD_check) + " " + str(mc_cycles_optimal_run))
