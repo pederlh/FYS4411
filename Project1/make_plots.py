66ddfc4a0689c04ce6e45f79a43d6a3db0dffd80
pederlh <36080334+pederlh@users.noreply.github.com>
2021-03-29 15:48:15 +0200

    Delete make_plots_copy.py

--- Project1/make_plots.py
+++ Project1/make_plots.py
@@ -3,6 +3,40 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 
+""" Script that both creates plots and performs blocking on local energy data sets """ 
+
+def block(x):
+    # preliminaries
+    n = len(x)
+    d = int(np.log2(n))
+    s, gamma = np.zeros(d), np.zeros(d)
+    mu = np.mean(x)
+
+    # estimate the auto-covariance and variances
+    # for each blocking transformation
+    for i in np.arange(0,d):
+        n = len(x)
+        # estimate autocovariance of x
+        gamma[i] = (n)**(-1)*np.sum( (x[0:(n-1)]-mu)*(x[1:n]-mu) )
+        # estimate variance of x
+        s[i] = np.var(x)
+        # perform blocking transformation
+        x = 0.5*(x[0::2] + x[1::2])
+
+    # generate the test observator M_k from the theorem
+    M = (np.cumsum( ((gamma/s)**2*2**np.arange(1,d+1)[::-1])[::-1] )  )[::-1]
+
+    # we need a list of magic numbers
+    q =np.array([6.634897,9.210340, 11.344867, 13.276704, 15.086272, 16.811894, 18.475307, 20.090235, 21.665994, 23.209251, 24.724970, 26.216967, 27.688250, 29.141238, 30.577914, 31.999927, 33.408664, 34.805306, 36.190869, 37.566235, 38.932173, 40.289360, 41.638398, 42.979820, 44.314105, 45.641683, 46.962942, 48.278236, 49.587884, 50.892181])
+
+    # use magic to determine when we should have stopped blocking
+    for k in np.arange(0,d):
+        if(M[k] < q[k]):
+            break
+    if (k >= d-1):
+        print("Warning: Use more data")
+    return mu, s[k]/2**(d-k)
+
 
 def make_plots(task):
     #Plots local energy and std for gridsearch brute force MC
@@ -723,41 +757,9 @@ def make_plots(task):
                 outfile.write(dts[i]+ " " + str(energy[i]) + " " + str(np.sqrt(variance[i])) + " " + str(time[i])+"\n")
 
 
-def block(x):
-    # preliminaries
-    n = len(x)
-    d = int(np.log2(n))
-    s, gamma = np.zeros(d), np.zeros(d)
-    mu = np.mean(x)
-
-    # estimate the auto-covariance and variances
-    # for each blocking transformation
-    for i in np.arange(0,d):
-        n = len(x)
-        # estimate autocovariance of x
-        gamma[i] = (n)**(-1)*np.sum( (x[0:(n-1)]-mu)*(x[1:n]-mu) )
-        # estimate variance of x
-        s[i] = np.var(x)
-        # perform blocking transformation
-        x = 0.5*(x[0::2] + x[1::2])
-
-    # generate the test observator M_k from the theorem
-    M = (np.cumsum( ((gamma/s)**2*2**np.arange(1,d+1)[::-1])[::-1] )  )[::-1]
-
-    # we need a list of magic numbers
-    q =np.array([6.634897,9.210340, 11.344867, 13.276704, 15.086272, 16.811894, 18.475307, 20.090235, 21.665994, 23.209251, 24.724970, 26.216967, 27.688250, 29.141238, 30.577914, 31.999927, 33.408664, 34.805306, 36.190869, 37.566235, 38.932173, 40.289360, 41.638398, 42.979820, 44.314105, 45.641683, 46.962942, 48.278236, 49.587884, 50.892181])
-
-    # use magic to determine when we should have stopped blocking
-    for k in np.arange(0,d):
-        if(M[k] < q[k]):
-            break
-    if (k >= d-1):
-        print("Warning: Use more data")
-    return mu, s[k]/2**(d-k)
-
-
-
 if __name__ == "__main__":
-    task = input("Which task to make plots for? \n"
-                + "Choices are 'b' (simplest), 'c' (importance sampling), 'd' (gradient descent evolution),'e' (no-repulsion) 'g' (repulsion) or 'h' (one body densities): ")
+    print("Possible commands: 'b' (simplest), 'c' (importance sampling), 'd' (gradient descent evolution), \n" \
+        + "'e' (no-repulsion), 'g' (repulsion), 'h' (one body densities) and 'deltat' (find optimal delta t)")
+    task = input("Which task to make plots for? ")
+    assert task in ["b", "c", "d", "e", "g", "h", "deltat"], "Input not recognized."
     make_plots(task)
