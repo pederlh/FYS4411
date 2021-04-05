ade801fa6c3b1717d4db1925c88a379c3c964f5e
Kaspara Skovli Gåsvær <kasparag@gmail.com>
2021-04-06 00:19:02 +0200

    Repo cleanup

--- Project1/Scripts/produce_data.py
+++ Project1/Scripts/produce_data.py
@@ -10,13 +10,13 @@ def move_files_ask_plot(task, path, filenames):
 
     # Take special care of one body density files
     if task == "e":
-        os.system("rm ./Results/1h_one_body_densities/One_body_density*.txt")
-        os.system("mv  One_body_density*.txt ./Results/1h_one_body_densities/")
+        os.system("rm ../Results/1h_one_body_densities/One_body_density*.txt")
+        os.system("mv  One_body_density*.txt ../Results/1h_one_body_densities/")
     # if type is d or g: move OBD
 
     if task == "g":
-        os.system("rm ./Results/1h_one_body_densities/Interaction_One_body_density*.txt")
-        os.system("mv  Interaction_One_body_density*.txt ./Results/1h_one_body_densities/")
+        os.system("rm ../Results/1h_one_body_densities/Interaction_One_body_density*.txt")
+        os.system("mv  Interaction_One_body_density*.txt ../Results/1h_one_body_densities/")
 
     if input("Press 'y' to make plots ") in ['y', 'Y']:
         make_plots(task)
@@ -27,7 +27,7 @@ print("Note: The data produced will overwrite previous results")
 """ Compilation """
 
 mac_linux_prompt = input("Run on macOS ('m') or Linux ('l')? ")
-
+os.chdir("../Src/")
 if mac_linux_prompt == 'l':
     os.system("g++  -o main.out Solver.cpp Psi.cpp main.cpp -std=c++11 -fopenmp")
 elif mac_linux_prompt == 'm':
@@ -35,7 +35,6 @@ elif mac_linux_prompt == 'm':
 else:
     print("Input not recognized. Aborting.")
     sys.exit(1)
-
 
 """ Choose task """
 task_prompt = input("Which task to run? Choices are 'b' (simplest), 'c' (importance sampling), 'e' (gradient descent + blocking + one body density) or 'g' (everything + repulsion): ")
@@ -61,7 +60,7 @@ if task_prompt == "b":
             os.system("./main.out " + str(n) + " " + str(dimentions) + " " + str(mc_cycles) + " " + str(c) + " " \
                                     + str(type_sampling) + " " + str(num_threads) + " " + str(OBD_check))
 
-    path = "./Results/1b_simple_noninteracting/"
+    path = "../Results/1b_simple_noninteracting/"
     filenames = "spherical*.txt"
     move_files_ask_plot("b", path, filenames)
 
@@ -80,7 +79,7 @@ if task_prompt == "c":
             os.system("./main.out " + str(n) + " " + str(dimentions) + " " + str(mc_cycles) + " " + str(c) + " " \
                                     + str(type_sampling) + " " + str(num_threads) + " " + str(OBD_check))
 
-    path = "./Results/1c_implementing_importance_sampling/"
+    path = "../Results/1c_implementing_importance_sampling/"
     filenames = "importance_spherical*.txt"
     move_files_ask_plot("c", path, filenames)
 
@@ -103,7 +102,7 @@ if task_prompt == "e":
         os.system("./main.out " + str(num_particles[i]) + " " + str(dimentions) + " " + str(mc_cycles) + " " + str(type_energy) + " " \
                                     + str(type_sampling) + " " + str(num_threads) + " " + str(OBD_check) + " " + str(mc_cycles_optimal_run) + " " + str(num_etas[i]))
 
-    path = "./Results/1e_implementing_gradient_descent_and_blocking/"
+    path = "../Results/1e_implementing_gradient_descent_and_blocking/"
     filenames = "OPTIMAL_ALPHA*.txt"
     move_files_ask_plot("e", path, filenames)
 
@@ -121,7 +120,7 @@ if task_prompt == "g":
         os.system("./main.out " + str(num_particles[i]) + " " + str(dimentions) + " " + str(mc_cycles) + " " + str(type_energy) + " " \
                                     + str(type_sampling) + " " + str(num_threads) + " " + str(OBD_check) + " " + str(mc_cycles_optimal_run) + " " + str(num_etas[i]))
 
-    path = "./Results/1g_implementing_repulsion/"
+    path = "../Results/1g_implementing_repulsion/"
     filenames = "INTERACTION_OPTIMAL_ALPHA*.txt"
     move_files_ask_plot("g", path, filenames)
 
@@ -137,12 +136,6 @@ if task_prompt == "delta_t":
 
     os.system("./main.out " + str(num_particles) + " " + str(dimentions) + " " + str(mc_cycles) + " " + str(type_energy) + " " + str(type_sampling) + " " + str(num_threads) + " " + str(OBD_check))
 
-    path = "./Results/delta_t_comparisons/"
+    path = "../Results/delta_t_comparisons/"
     filename = "dt_run_" + dt + "_ana.txt"
     os.system("mv " + filename + " " + path)
-
-
-
-    """
-    Husk å fikse delta t !!!!!!!!!!!!!!!
-    """
