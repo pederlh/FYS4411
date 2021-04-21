9062b032e6f61b6fef453f7b502962613ff879f9
Kaspara Skovli Gåsvær <kasparag@gmail.com>
2021-04-21 12:56:46 +0200

    a bit more work on boltzmann

--- Project2/BoltzmannMachine.hpp
+++ Project2/BoltzmannMachine.hpp
@@ -16,14 +16,32 @@ using namespace arma;
 
 class BoltzmannMachine {
 
+    /*
+    Class for NQS wave function.
+    */
+
+private:
+    void (BoltzmannMachine::*optimizer)();
+
+    //For ADAM;
+    void ADAM();
+    double beta1_, beta2_, epsilon_, alpha_batch_, epsilon_batch_;
+    double sigma_, eta_;
+    cube mom_w_, second_mom_w_;
+    vec mom_b_, second_mom_b_;
+    mat mom_a_, second_mom_a_;
+
 public:
-    cube w_;
-    mat a_;
-    vec b_;
+    cube w_, dw_;
+    mat a_, da_;
+    vec b_, db_;
     int D_, N_, hidden_nodes_;
     random_device rd_;
 
-void Initialize_Parameters(int dimentions, int num_particles);
+    void Initialize_Parameters(int dimentions, int num_particles, double eta);
+
+
+
 };
 
 #endif
