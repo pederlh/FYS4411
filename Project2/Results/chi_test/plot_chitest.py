e1547d33b8817e491499a2570f4ef804d0c9cb14
pederlh <36080334+pederlh@users.noreply.github.com>
2021-06-04 16:28:01 +0200

    fix chi test

--- Project2/Results/chi_test/plot_chitest.py
+++ Project2/Results/chi_test/plot_chitest.py
@@ -55,54 +55,61 @@ if nnt == 0:
     print("nt = 0!!!")
 
 if nc == 1:
-    mc_c[0] = math.log2(int(mc_chi)*4)
+    mc_c[0] = int(mc_chi)*4
     means_c[0] = float(means_chi)
     stds_c[0] = float(stds_chi)
 else:
     for i in range(nc):
-        mc_c[i] = math.log2(int(mc_chi[i])*4)
+        mc_c[i] = int(mc_chi[i])*4
         means_c[i] = float(means_chi[i])
         stds_c[i] = float(stds_chi[i])
 
 if nt == 1:
-    mc_t[0] = math.log2(int(mc_test)*4)
+    mc_t[0] = int(mc_test)*4
     means_t[0] = float(means_test)
     stds_t[0] = float(stds_test)
 else:
     for i in range(nt):
-        mc_t[i] = math.log2(int(mc_test[i])*4)
+        mc_t[i] = int(mc_test[i])*4
         means_t[i] = float(means_test[i])
         stds_t[i] = float(stds_test[i])
 
 if nnc == 1:
-    mc_NC_c[0] = math.log2(int(mc_NC_chi)*4)
+    mc_NC_c[0] = int(mc_NC_chi)*4
     means_NC_c[0] = float(means_NC_chi)
     stds_NC_c[0] = float(stds_NC_chi)
 else:
     for i in range(nnc):
-        mc_NC_c[i] = math.log2(int(mc_NC_chi[i])*4)
+        mc_NC_c[i] = int(mc_NC_chi[i])*4
         means_NC_c[i] = float(means_NC_chi[i])
         stds_NC_c[i] = float(stds_NC_chi[i])
 
 
 if nnt == 1:
-    mc_NC_t[0] = math.log2(int(mc_NC_test)*4)
+    mc_NC_t[0] = int(mc_NC_test)*4
     means_NC_t[0] = float(means_NC_test)
     stds_NC_t[0] = float(stds_NC_test)
 else:
     for i in range(nnt):
-        mc_NC_t[i] = math.log2(int(mc_NC_test[i])*4)
+        mc_NC_t[i] = int(mc_NC_test[i])*4
         means_NC_t[i] = float(means_NC_test[i])
         stds_NC_t[i] = float(stds_NC_test[i])
 
 
-plt.errorbar(mc_c,means_c,yerr=stds_c, fmt = "or",capsize=5, elinewidth=1,markeredgewidth=1, label = "Chi")
-plt.errorbar(mc_t,means_t,yerr=stds_t, fmt = "ob",capsize=5, elinewidth=1,markeredgewidth=1, label = "Test")
-plt.plot(mc_NC_c,means_NC_c,"rx", label = "Chi not converged")
-plt.plot(mc_NC_t,means_NC_t,"bx", label = "Test not converged")
-plt.axhline(y=3.0, color='k', linestyle='-', label = "Analytical")
-plt.legend()
-plt.xlabel("log2(MC-cycles)")
+plt.errorbar(mc_c,means_c,yerr=stds_c, fmt = "or",capsize=5, elinewidth=1,markeredgewidth=1, label = "Increase $\chi$")
+plt.errorbar(mc_t,means_t,yerr=stds_t, fmt = "ob",capsize=5, elinewidth=1,markeredgewidth=1, label = "Ignore $H_1$")
+plt.semilogx(mc_NC_c,means_NC_c,"rx", base=2)
+plt.semilogx(mc_NC_t,means_NC_t,"bx", base=2)
+plt.axhline(y=3.0, color='k', linestyle='--', alpha=0.8, label = r"Analytical $\langle E_L \rangle$")  
+
+# Fix order of legends
+handles, labels = plt.gca().get_legend_handles_labels()
+order = [1,0,2]
+plt.legend([handles[idx] for idx in order],[labels[idx] for idx in order], ncol=2)
+
+plt.xlabel("Monte Carlo cycles")
 plt.ylabel(r"$\langle E_L \rangle$")
+plt.ylim([2.85, 3.52])
 plt.tight_layout()
+plt.savefig("chi_test_vs_mc.pdf")
 plt.show()
